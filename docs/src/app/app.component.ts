9e0725a8fff8faab7ba298fdf86f5129914499e7
Alexey Matveev <as.luden@gmail.com>
2016-12-28 20:01:43 +0400

    Remove redundant HashLocationStrategy fromAppComponent

--- docs/src/app/app.component.ts
+++ docs/src/app/app.component.ts
@@ -1,19 +1,15 @@
-import {HashLocationStrategy, Location, LocationStrategy} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  providers: [Location, {provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  location: Location;
   title = 'app works!';
   
-  constructor(location: Location) { 
-    this.location = location; 
-  }
+  constructor() { }
 
   
 }
