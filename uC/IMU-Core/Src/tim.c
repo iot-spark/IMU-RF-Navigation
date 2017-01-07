235f4f37500afb2555e7525a4645f0928be2a3e7
Alexey Matveev <as.luden@gmail.com>
2017-01-07 20:45:19 +0400

    Change TIM6 frequency to 100 Hz

--- uC/IMU-Core/Src/tim.c
+++ uC/IMU-Core/Src/tim.c
@@ -57,9 +57,9 @@ void MX_TIM6_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig;
 
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 0;
+  htim6.Init.Prescaler = 99;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 8399; // 10 kHz
+  htim6.Init.Period = 8399; // 100 Hz
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
     Error_Handler();
