aaec3b0c3ce69054f76e9fcc4b1420436c20470c
Alexey Matveev <as.luden@gmail.com>
2017-01-30 23:00:48 +0400

    Implement 100k random chart with small fonts and gaps

--- wpf/kf_ga_modelling/kf_ga_modelling/Behaviors/ZedGraphBehavior.cs
+++ wpf/kf_ga_modelling/kf_ga_modelling/Behaviors/ZedGraphBehavior.cs
@@ -9,6 +9,9 @@ namespace kf_ga_modelling.Behaviors
     {
         #region Fields
         ZedGraphControl _zg = null;
+        LineItem _lineChart = null;
+        Random _rnd = new Random();
+        int _chartLng = 100000;
         #endregion
 
         public ZedGraphBehavior()
@@ -30,18 +33,38 @@ namespace kf_ga_modelling.Behaviors
             _zg.Name = "zedGraphControl1";
             _zg.Size = new System.Drawing.Size(680, 414);
             _zg.TabIndex = 0;
+            //_zg.Font = new System.Drawing.Font("Segoe UI", 0.2f);
 
             _zg.IsShowPointValues = true;
             _zg.GraphPane.Title = "Test Case for C#";
-            double[] x = new double[100];
-            double[] y = new double[100];
+            _zg.GraphPane.FontSpec.Family = "Segoe UI";
+            _zg.GraphPane.FontSpec.Size = 2.0f * (_zg.Size.Height / 100.0f);
+
+            _zg.GraphPane.XAxis.TitleFontSpec.Family = "Segoe UI";
+            _zg.GraphPane.XAxis.TitleFontSpec.Size = 1.5f * (_zg.Size.Height / 100.0f);
+            _zg.GraphPane.XAxis.ScaleFontSpec.Family = "Segoe UI";
+            _zg.GraphPane.XAxis.ScaleFontSpec.Size = 1.5f * (_zg.Size.Height / 100.0f);
+
+            _zg.GraphPane.YAxis.TitleFontSpec.Family = "Segoe UI";
+            _zg.GraphPane.YAxis.TitleFontSpec.Size = 1.5f * (_zg.Size.Height / 100.0f);
+            _zg.GraphPane.YAxis.ScaleFontSpec.Family = "Segoe UI";
+            _zg.GraphPane.YAxis.ScaleFontSpec.Size = 1.5f * (_zg.Size.Height / 100.0f);
+
+            _zg.GraphPane.Legend.FontSpec.Family = "Segoe UI";
+            _zg.GraphPane.Legend.FontSpec.Size = 1.5f * (_zg.Size.Height / 100.0f);
+
+            _zg.GraphPane.PaneGap = 0.1f * (_zg.Size.Height / 100.0f);
+
+            double[] x = new double[_chartLng];
+            double[] y = new double[_chartLng];
             int i;
-            for (i = 0; i < 100; i++)
+            for (i = 0; i < _chartLng; i++)
             {
-                x[i] = (double)i / 100.0 * Math.PI * 2.0;
-                y[i] = Math.Sin(x[i]);
+                x[i] = (double)i / 1000.0;
+                y[i] = _rnd.NextDouble() * 10.0;
             }
-            _zg.GraphPane.AddCurve("Sine Wave", x, y, System.Drawing.Color.Red, SymbolType.None);
+
+            _lineChart = _zg.GraphPane.AddCurve("Sine Wave", x, y, System.Drawing.Color.Red, SymbolType.None);
             _zg.AxisChange();
             _zg.Invalidate();
         }
