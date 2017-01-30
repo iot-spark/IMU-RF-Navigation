0f5d49c5e0eb11e1a2b033acf7d7168b761a78d0
Alexey Matveev <as.luden@gmail.com>
2017-01-30 22:14:41 +0400

    Implement ZedGrap test solution

--- wpf/kf_ga_modelling/kf_ga_modelling/MainWindow.xaml.cs
+++ wpf/kf_ga_modelling/kf_ga_modelling/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ZedGraph;
 
 namespace kf_ga_modelling
 {
@@ -20,9 +10,42 @@ namespace kf_ga_modelling
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+        private ZedGraphControl _zg;
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 
+            // zedGraphControl1
+            // 
+            _zg = new ZedGraphControl();
+
+            zgHost.Child = _zg;
+
+            _zg.Location = new System.Drawing.Point(0, 0);
+            _zg.Name = "zedGraphControl1";
+            _zg.Size = new System.Drawing.Size(680, 414);
+            _zg.TabIndex = 0;
+
+            _zg.IsShowPointValues = true;
+            _zg.GraphPane.Title = "Test Case for C#";
+            double[] x = new double[100];
+            double[] y = new double[100];
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                x[i] = (double)i / 100.0 * Math.PI * 2.0;
+                y[i] = Math.Sin(x[i]);
+            }
+            _zg.GraphPane.AddCurve("Sine Wave", x, y, Color.Red, SymbolType.None);
+            _zg.AxisChange();
+            _zg.Invalidate();
         }
     }
 }
---
+++ wpf/kf_ga_modelling/kf_ga_modelling/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace kf_ga_modelling.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        private string _recordsFile = "N/A";
+        #endregion
+
+
+        public string RecordsFile {
+            get { return _recordsFile; }
+            set {
+                _recordsFile = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
